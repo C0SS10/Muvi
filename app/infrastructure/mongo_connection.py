f544f51c5db13bfaa71aefaf0bf7a904d1ca9c57
Esteban <estebancg12@hotmail.com>
2025-07-22 10:54:26 -0500

    Load variables based on the current environment 👁️‍🗨️

--- app/infrastructure/mongo_connection.py
+++ app/infrastructure/mongo_connection.py
@@ -3,7 +3,13 @@ from pymongo.database import Database
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Detect enviroment
+env = os.getenv("ENVIRONMENT", "local")
+
+if env == "test":
+    load_dotenv(".env.test")
+else:
+    load_dotenv(".env.local")
 
 MONGODB_URI = os.getenv("MONGODB_URL")
 DATABASE_NAME = os.getenv("MONGODB_DATABASE_NAME")
