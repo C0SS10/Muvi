39fdeb0aefd007a0bac7bbb47b18f8cf1dd3e510
Esteban <estebancg12@hotmail.com>
2025-07-22 10:53:39 -0500

    Refactor movie addition logic and handle value errors 🧰

--- app/domain/services/movie_services.py
+++ app/domain/services/movie_services.py
@@ -1,12 +1,17 @@
 from app.domain.models.movie import Movie
 from typing import Tuple, Dict
+from pydantic import ValidationError
 from app.domain.repositories.movie_repositories import insert_movie
 
 
 def add_movie(movie: Dict) -> Tuple[str, int]:
-    movie_validated = Movie(**movie)
-    movie_dict = movie_validated.model_dump(exclude={"id"})
+    try:
+        movie_validated = Movie(**movie)
+        movie_dict = movie_validated.model_dump(exclude={"id"})
+        movie_id = insert_movie(movie_dict)
 
-    movie_id = insert_movie(movie_dict)
+        return f"Movie created with ID: {movie_id}", 201
+    except ValidationError as error:
+        message = error.errors()[0].get("msg", "Invalid data")
 
-    return f"Movie created with ID: {movie_id}", 201
+        return message, 422
