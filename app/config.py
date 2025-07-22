c394be63358ec38a271a1381ef23f2ef116e7c38
Esteban <estebancg12@hotmail.com>
2025-07-22 12:17:09 -0500

    Add logging configuration and display banner on startup 🎨

--- app/config.py
+++ app/config.py
@@ -1,10 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes.router import router
+import logging
 
 def create_app() -> Flask:
     application = Flask(__name__)
     CORS(application)
     application.register_blueprint(router)
 
+    # Desactiva el banner de Flask (opcional)
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+
     return application
+
+# Banner to display that shows 'MUVI'
+BANNER: str = r"""
+_|      _|  _|    _|  _|      _|  _|_|_|  
+_|_|  _|_|  _|    _|  _|      _|    _|    
+_|  _|  _|  _|    _|  _|      _|    _|    
+_|      _|  _|    _|    _|  _|      _|    
+_|      _|    _|_|        _|      _|_|_|
+
+                MUVI
+"""
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from app.config import create_app
+import os
+from colorama import init, Fore, Style
+from app.config import create_app, BANNER
 
 
 if __name__ == "__main__":
     application = create_app()
-    application.run(debug=True, threaded=True)
+    host = os.getenv("HOST_IP", "127.0.0.1")
+    port = int(os.getenv("HOST_PORT", 5000))
+
+    init(autoreset=True)
+    print(Fore.GREEN + Style.DIM + BANNER)
+    print(Fore.YELLOW + Style.BRIGHT + f"Running on http://{host}:{port}")
+
+    application.run(host=host, port=port, debug=True, use_reloader=False)
