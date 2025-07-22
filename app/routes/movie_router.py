540889131d7fab84f093f64a6a9b948bf6cfb6a2
Esteban <estebancg12@hotmail.com>
2025-07-22 10:53:39 -0500

    Refactor movie addition logic and handle value errors 🧰

--- app/routes/movie_router.py
+++ app/routes/movie_router.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, request, Response, jsonify
 from typing import Tuple
-
 from app.domain.services.movie_services import add_movie
 
 movie_router = Blueprint('movies', __name__)
 
 @movie_router.route('/', methods=['POST'])
-def create_movie() -> Tuple[str, int] | Tuple[Response, int]:
-    movie = request.get_json()
-    message, status_code = add_movie(movie)
-
-    return jsonify({"message": message}), status_code
+def create_movie() -> Tuple[Response, int]:
+        movie = request.get_json()
+        response_message, status_code = add_movie(movie)
+        return jsonify({"message": response_message}), status_code
