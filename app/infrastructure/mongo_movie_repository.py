b592ce6d684b6e68b50f3705d1d4711066381435
Esteban <estebancg12@hotmail.com>
2025-07-29 09:37:45 -0500

    Add get_all_movies functionality to repos ✅

--- app/infrastructure/mongo_movie_repository.py
+++ app/infrastructure/mongo_movie_repository.py
@@ -1,3 +1,4 @@
+from app.domain.models.movie import Movie
 from app.domain.repositories.movie_repository import MovieRepository
 from app.infrastructure.mongo_connection import database
 from typing import Dict, List
@@ -10,3 +11,7 @@ class MongoMovieRepository(MovieRepository):
     def insert_many_movies(self, movies_dict: List[Dict]) -> List[str]:
         movies_identifiers = database.movies.insert_many(movies_dict)
         return [str(id) for id in movies_identifiers.inserted_ids]
+    
+    def get_all_movies(self) -> List[Movie]:
+        documents = database.movies.find()
+        return [Movie.from_mongo(document) for document in documents]
