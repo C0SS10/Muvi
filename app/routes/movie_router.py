e3b9239d3c5cfc2b1e4fd67ea6933cbe2ed46355
Esteban <estebancg12@hotmail.com>
2025-07-29 09:59:39 -0500

    Use a constant for internal server error message ❗

--- app/routes/movie_router.py
+++ app/routes/movie_router.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, redirect, request, jsonify, url_for
 from werkzeug.wrappers.response import Response
 from typing import Tuple
-from app.domain.models.movie import Movie
 from app.domain.services.movie_services import MovieService
 from app.infrastructure.mongo_movie_repository import MongoMovieRepository
+
+INTERNAL_SERVER_ERROR_MSG = "Internal server error"
 
 movie_router = Blueprint('movies', __name__)
 
@@ -23,7 +24,7 @@ def create_movie() -> Tuple[Response, int] | Response:
                 response_message, status_code = movie_service.add_movie(movie)
                 return jsonify({"message": response_message}), status_code
         except Exception:
-                return jsonify({"message": "Internal server error"}), 500
+                return jsonify({"message": INTERNAL_SERVER_ERROR_MSG}), 500
 
 @movie_router.route('/insert-many', methods=['POST'])
 def create_movies() -> Tuple[Response, int] | Response:
@@ -41,7 +42,7 @@ def create_movies() -> Tuple[Response, int] | Response:
                 response_message, status_code = movie_service.add_many_movies(movies)
                 return jsonify({"message": response_message}), status_code
         except Exception:
-                return jsonify({"message": "Internal server error"}), 500
+                return jsonify({"message": INTERNAL_SERVER_ERROR_MSG}), 500
 
 @movie_router.route('', methods=['GET'])
 def get_movies() -> Tuple[Response, int]:
@@ -51,4 +52,4 @@ def get_movies() -> Tuple[Response, int]:
                 movies = [movie.model_dump() for movie in movies_raw]
                 return jsonify(movies), 200
         except Exception:
-                return jsonify({"message": "Internal server error"}), 500
+                return jsonify({"message": INTERNAL_SERVER_ERROR_MSG}), 500
