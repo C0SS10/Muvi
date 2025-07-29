5351212816c2dbfcdd99c94a963aab7a0692c398
Esteban <estebancg12@hotmail.com>
2025-07-29 09:37:45 -0500

    Add get_all_movies functionality to repos ✅

--- app/domain/repositories/movie_repository.py
+++ app/domain/repositories/movie_repository.py
@@ -1,6 +1,8 @@
 from typing import Dict, List
 from abc import ABC, abstractmethod
 
+from app.domain.models.movie import Movie
+
 
 class MovieRepository(ABC):
     @abstractmethod
@@ -9,4 +11,8 @@ class MovieRepository(ABC):
 
     @abstractmethod
     def insert_many_movies(self, movies_dict: List[Dict]) -> List[str]:
+        pass
+
+    @abstractmethod
+    def get_all_movies(self) -> List[Movie]:
         pass
